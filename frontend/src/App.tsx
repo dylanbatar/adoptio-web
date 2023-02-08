fdc39fe3f72d85c5315dfe44f23febab77a79ede
dylanbatar <dylanbatar2@gmail.com>
2023-02-07 22:45:53 -0500

    Add tailwind config

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,23 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={logo} className="App-logo mb-5" alt="logo" />
+        <h1 className="text-3xl font-bold underline">Hello world! reloadddd</h1>
       </header>
     </div>
   );
