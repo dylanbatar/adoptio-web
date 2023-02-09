b74cf9495a4c73a178e6d381b790fc07af2b9c09
dylanbatar <dylanbatar2@gmail.com>
2023-02-09 03:27:55 -0500

    fix pet page responsive

--- frontend/src/screens/pet/index.tsx
+++ frontend/src/screens/pet/index.tsx
@@ -14,15 +14,15 @@ export const PetPage = () => {
 
   return (
     <section className="py-20 container mx-auto">
-      <div className="flex justify-around">
-        <div className="w-4/12 rounded-lg overflow-hidden relative shadow-lg hover:shadow-sky-300/40 my-4 lg:my-0 ">
+      <div className="flex justify-around px-4 flex-col md:flex-row">
+        <div className="w-full md:w-4/12 rounded-lg overflow-hidden relative shadow-lg hover:shadow-sky-300/40 my-4 lg:my-0 ">
           <img
             className="w-full h-full"
             alt={pet?.name}
             src={`${process.env.PUBLIC_URL}/assets/${pet?.imageFileName}`}
           />
         </div>
-        <div className="w-6/12">
+        <div className="w-full md:w-6/12">
           <h2 className="font-medium text-5xl text-indigo-900  animate__animated animate__fadeInUp">
             Information
           </h2>
